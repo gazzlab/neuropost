0156a73cbb7e67e3fe3d00b940186c905f4dee22
Simon Forman <forman.simon@gmail.com>
2013-02-25 19:49:18 -0800

    "force" use of server interpreter (not local JS)
    and I removed the copyright notice until we can add one for GazzLab.

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-#    Copyright © 2013 Simon Forman
-#
-#    This file is part of Xerblin.
-#
-#    Xerblin is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Xerblin is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Xerblin.  If not, see <http://www.gnu.org/licenses/>.
-#
 import os, json
 from types import FunctionType
 from flask import Flask, render_template, Response, request
